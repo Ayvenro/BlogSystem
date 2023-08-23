17fbb49f1dee7d2d8e7812cbdd38d39a38af553e
Ayvenro <46055882+Ayvenro@users.noreply.github.com>
2023-08-23 14:17:11 +0300

    Created application project

---
+++ BlogSystem.Application/Contracts/Persistence/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using BlogSystem.Domain.Entities;
+
+namespace BlogSystem.Application.Contracts.Persistence
+{
+	public interface ICategoryRepository : IGenericAsyncRepository<Category>
+	{
+
+	}
+}
---
+++ BlogSystem.Application/Contracts/Persistence/IGenericAsyncRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace BlogSystem.Application.Contracts.Persistence
+{
+	public interface IGenericAsyncRepository<T> where T : class
+	{
+		Task<T> GetByIdAsync(int id);
+		Task<IReadOnlyList<T>> GetAllAsync();
+		Task<T> AddAsync(T entity);
+		Task UpdateAsync(T entity);
+		Task DeleteAsync(T entity);
+
+	}
+}
