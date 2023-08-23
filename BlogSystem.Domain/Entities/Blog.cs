22e040d2f54452e949ba2289b777202d2aafa0a5
Ayvenro <46055882+Ayvenro@users.noreply.github.com>
2023-08-23 14:13:20 +0300

    Created domain project and data model

---
+++ BlogSystem.Domain/Entities/Blog.cs
@@ -0,0 +1,13 @@
+﻿using BlogSystem.Domain.Common;
+
+namespace BlogSystem.Domain.Entities
+{
+	public class Blog : AuditableEntity
+	{
+        public Guid Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public Guid CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
---
+++ BlogSystem.Domain/Entities/Category.cs
@@ -0,0 +1,11 @@
+﻿using BlogSystem.Domain.Common;
+
+namespace BlogSystem.Domain.Entities
+{
+	public class Category : AuditableEntity
+	{
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public ICollection<Blog> Blogs { get; set; }
+    }
+}
