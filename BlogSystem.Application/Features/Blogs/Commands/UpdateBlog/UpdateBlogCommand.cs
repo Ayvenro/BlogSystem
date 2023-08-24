1be77400bb9afec234a02b1af9989e228a9fdecc
Ayvenro <46055882+Ayvenro@users.noreply.github.com>
2023-08-24 15:16:42 +0300

    Added UpdateBlog command

---
+++ BlogSystem.Application/Features/Blogs/Commands/UpdateBlog/UpdateBlogCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace BlogSystem.Application.Features.Blogs.Commands.UpdateBlog
+{
+	public class UpdateBlogCommand : IRequest
+	{
+		public Guid Id { get; set; }
+		public string Title { get; set; } = string.Empty;
+		public string Content { get; set; } = string.Empty;
+		public Guid CategoryId { get; set; }
+	}
+}
---
+++ BlogSystem.Application/Features/Blogs/Commands/UpdateBlog/UpdateBlogCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using BlogSystem.Application.Contracts.Persistence;
+using BlogSystem.Domain.Entities;
+using MediatR;
+
+namespace BlogSystem.Application.Features.Blogs.Commands.UpdateBlog
+{
+	public class UpdateBlogCommandHandler : IRequestHandler<UpdateBlogCommand>
+	{
+		private readonly IGenericAsyncRepository<Blog> _blogRepository;
+		private readonly IMapper _mapper;
+
+		public UpdateBlogCommandHandler(IGenericAsyncRepository<Blog> blogRepository, IMapper mapper)
+		{
+			_blogRepository = blogRepository;
+			_mapper = mapper;
+		}
+
+		public async Task Handle(UpdateBlogCommand request, CancellationToken cancellationToken)
+		{
+			var blogToUpdate = await _blogRepository.GetByIdAsync(request.Id);
+			_mapper.Map(request, blogToUpdate, typeof(UpdateBlogCommand), typeof(Blog));
+			await _blogRepository.UpdateAsync(blogToUpdate);
+			return;
+		}
+	}
+}
