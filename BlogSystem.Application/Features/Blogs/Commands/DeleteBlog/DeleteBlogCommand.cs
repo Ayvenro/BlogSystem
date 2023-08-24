f67d47b2d0c066daae308c2e9ab2441ee898d256
Ayvenro <46055882+Ayvenro@users.noreply.github.com>
2023-08-24 15:18:40 +0300

    Added delete blog command

---
+++ BlogSystem.Application/Features/Blogs/Commands/DeleteBlog/DeleteBlogCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace BlogSystem.Application.Features.Blogs.Commands.DeleteBlog
+{
+	public class DeleteBlogCommand : IRequest
+	{
+        public Guid Id { get; set; }
+    }
+}
---
+++ BlogSystem.Application/Features/Blogs/Commands/DeleteBlog/DeleteBlogCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using BlogSystem.Application.Contracts.Persistence;
+using BlogSystem.Domain.Entities;
+using MediatR;
+
+namespace BlogSystem.Application.Features.Blogs.Commands.DeleteBlog
+{
+	public class DeleteBlogCommandHandler : IRequestHandler<DeleteBlogCommand>
+	{
+		private readonly IGenericAsyncRepository<Blog> _blogRepository;
+		private readonly IMapper _mapper;
+
+		public DeleteBlogCommandHandler(IGenericAsyncRepository<Blog> blogRepository, IMapper mapper)
+		{
+			_blogRepository = blogRepository;
+			_mapper = mapper;
+		}
+
+		public async Task Handle(DeleteBlogCommand request, CancellationToken cancellationToken)
+		{
+			var blogToDelete = await _blogRepository.GetByIdAsync(request.Id);
+			await _blogRepository.DeleteAsync(blogToDelete);
+			return;
+		}
+	}
+}
