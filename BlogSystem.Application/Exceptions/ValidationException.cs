34f359ce8e35828abd9ab0be712018618765ae63
Ayvenro <46055882+Ayvenro@users.noreply.github.com>
2023-08-25 13:33:41 +0300

    Added exception

---
+++ BlogSystem.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation.Results;
+
+namespace BlogSystem.Application.Exceptions
+{
+	public class ValidationException : Exception
+	{
+		public List<string> Errors { get; set; }
+
+		public ValidationException(ValidationResult validationResult)
+		{
+			Errors = new List<string>();
+			foreach (var error in validationResult.Errors)
+			{ 
+				Errors.Add(error.ErrorMessage); 
+			}
+		}
+	}
+}
